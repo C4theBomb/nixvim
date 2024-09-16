78ba42e8494ac27926d86d4082ef5cd17dfb279e
C4 Patino <c4patino@gmail.com>
2024-09-15 19:19:42 -0500

    Added old mappings to neovim configuration

--- config/mappings.nix
+++ config/mappings.nix
@@ -1,5 +1,98 @@
 {
     keymaps = [
+        { 
+            mode = "n";
+            key = ";";
+            action = ":";
+            options.desc = "General Enter command mode";
+        }
+        { 
+            mode = "n";
+            key = "<Esc>";
+            action = "<cmd>noh<cr>";
+            options.desc = "General Clear highlights";
+        }
+        { 
+            mode = "n";
+            key = "<C-s>";
+            action = "<cmd>w<CR>";
+            options.desc = "General File save";
+        }
+
+        { 
+            mode = "n";
+            key = "<C-u>";
+            action = "<C-u>zz";
+            options.desc = "Vertical Scroll up";
+        }
+        { 
+            mode = "n";
+            key = "<C-d>";
+            action = "<C-d>zz";
+            options.desc = "Vertical Scroll down";
+        }
+
+        { 
+            mode = "n";
+            key = "n";
+            action = "nzzzv";
+            options.desc = "Find/Replace Move to next match";
+        }
+        { 
+            mode = "n";
+            key = "N";
+            action = "Nzzzv";
+            options.desc = "Find/Replace Move to previous match";
+        }
+        {
+            mode = "n";
+            key = "<leader>s";
+            action = ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>";
+            options.desc = "Find/Replace Replace current word";
+        }
+
+        { 
+            mode = "v";
+            key = "J";
+            action = ":m '>+1<CR>gv=gv";
+            options.desc = "Editing Move line down";
+        }
+        { 
+            mode = "v";
+            key = "K";
+            action = ":m '<-2<CR>gv=gv";
+            options.desc = "Editing Move line down";
+        }
+
+        {
+            mode = ["n" "v"];
+            key = "<leader>y";
+            action = "\"+y";
+            options.desc = "Editing Copy to system clipboard";
+        }
+        {
+            mode = "v";
+            key = "<leader>Y";
+            action = "\"+Y";
+            options.desc = "Editing Copy line to system clipboard";
+        }
+
+        {
+            mode = "v";
+            key = "<leader>p";
+            action = "\"_dP";
+            options.desc = "Editing Replace text with paste buffer";
+        }
+        {
+            mode = ["n" "v"];
+            key = "<leader>d";
+            action = "\"_d";
+            options.desc = "";
+        }
+
+
+
+
 
     ];
 }
