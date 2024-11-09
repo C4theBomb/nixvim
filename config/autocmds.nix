d934d39470a9dac4e79b243f384c33cbc9335625
C4 Patino <c4patino@gmail.com>
2024-11-09 17:04:37 -0600

    Added autocommand for switching the width of nix tabs

--- config/autocmds.nix
+++ config/autocmds.nix
@@ -4,5 +4,10 @@
 			event = "TextYankPost";
 			callback.__raw = ''function() vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40, }) end'';
         }
+		{
+			event = ["FileType"];
+			pattern = ["*.nix"];
+			callback.__raw = ''vim.opt_local.tabstop = 2; vim.opt_local.shiftwidth = 2; vim.opt_local.expandtab = true;'';
+		}
 	];
 }
